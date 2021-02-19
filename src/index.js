1aaf5e1157acf579a262d9da08fb317162c972a9
Nastya-Ivanova <tkacheva123@gmail.com>
2021-02-19 21:08:38 +0300

    added problem solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let map = new Map(bracketsConfig);
+
+  let arr = str.split('');
+
+  let data = [];
+
+  if(str.length%2 > 0 || str.length == 0){
+    return false;
+  }
+
+  for(let i = 0; i < arr.length; i++){
+    if(map.has(arr[i]) && !(data.includes(map.get(arr[i])))){
+      data.push(arr[i]);
+    }
+    else if(arr[i] == map.get(data[data.length-1])){
+      data.pop();
+    }
+    else return false;
+  }
+  return data.length > 0 ? false : true;
+ }
